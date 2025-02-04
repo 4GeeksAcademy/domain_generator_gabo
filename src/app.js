46fae6d3665066875a6c003b8b3cf4731fcf564c
Gabo <gaboramirezdev@gmail.com>
2025-02-03 18:29:03 -0600

    Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  const printDomainsCom=()=>{
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+          
+        }
+        
+      }
+      
+    }
+  }
+
+  //Hacks domains
+const printDomainsHack=()=>{
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k].substring(0,noun[k].length-2)}.${noun[k].substring(noun[k].length-2,noun[k].length)}`);
+        
+      }
+      
+    }
+    
+  }
+}
+  
+printDomainsCom();
+printDomainsHack();
